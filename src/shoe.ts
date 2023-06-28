bf36bef59d003e89cb6b7302a5416585303d3a7a
votienkhiem <votienkhiem@users.noreply.github.com>
2023-06-28 13:09:15 +0700

    a

---
+++ src/shoe.ts
@@ -0,0 +1,44 @@
+export interface Shoe {
+  id: number,
+  name: string,
+  price: number,
+  description: string,
+  image: string
+}
+export const shoes = [
+  {
+    id: 1,
+    name: 'Converse',
+    price: 1.99,
+    description: 'max size',
+    image: '/assets/image/shoe1.jpg'
+  },
+  {
+    id: 2,
+    name: 'Converse',
+    price: 2.99,
+    description: 'max size',
+    image: ''
+  },
+  {
+    id: 3,
+    name: 'Converse',
+    price: 3.99,
+    description: 'max size',
+    image: ''
+  },
+  {
+    id: 4,
+    name: 'Converse',
+    price: 4.99,
+    description: 'max size',
+    image: ''
+  },
+  {
+    id: 5,
+    name: 'Converse',
+    price: 5.99,
+    description: 'max size',
+    image: ''
+  },
+]

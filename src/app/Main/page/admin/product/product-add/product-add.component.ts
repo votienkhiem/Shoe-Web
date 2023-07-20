3317481c1f584bd580b4bcf4954c46109dd906cd
votienkhiem <votienkhiem@users.noreply.github.com>
2023-07-20 19:52:59 +0700

    update html edit

--- src/app/Main/page/admin/product/product-add/product-add.component.ts
+++ src/app/Main/page/admin/product/product-add/product-add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-add',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent {
+  public addF: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    image: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    quantity: new FormControl('1', Validators.required),
+    brand: new FormControl('', Validators.required),
+    status: new FormControl('Status', Validators.required),
+    detail: new FormControl('', Validators.required),
+    history: new FormControl('', Validators.required),
+    color: new FormControl('Color', Validators.required),
+    size: new FormControl('', Validators.required),
+  })
 
+
+  addSubmit(): void {
+    if (this.addF.valid) {
+
+      console.log(this.addF.value);
+    }
+  }
 }
--- src/app/Main/page/admin/product/product-edit/product-edit.component.ts
+++ src/app/Main/page/admin/product/product-edit/product-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent {
+  public editF: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    image: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    quantity: new FormControl('1', Validators.required),
+    brand: new FormControl('', Validators.required),
+    status: new FormControl('Status', Validators.required),
+    detail: new FormControl('', Validators.required),
+    history: new FormControl('', Validators.required),
+    color: new FormControl('Color', Validators.required),
+    size: new FormControl('', Validators.required),
+  })
 
+
+  editSubmit(): void {
+    if (this.editF.valid) {
+
+      console.log(this.editF.value);
+    }
+  }
 }

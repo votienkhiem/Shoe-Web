c94635895b0bea4ab3ff865a9d072ebee8193864
votienkhiem <votienkhiem@users.noreply.github.com>
2023-07-20 13:59:47 +0700

    add form CRUD

--- src/app/Main/page/admin/admin.component.ts
+++ src/app/Main/page/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
+import { DataService } from 'src/app/service/data.service';
+import { ShoeingService } from 'src/app/service/shoeing.service';
+import { Shoe } from 'src/shoe';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  shoes: Shoe[] = [];
+  ngOnInnit(): void {
 
+  }
+  constructor(
+    private shoeService: ShoeingService,
+    private cart: CartService,
+    private data: DataService
+  ) {
+    this.shoeService.getAllShoes().then((arrShoes: Shoe[]) => {
+      this.shoes = arrShoes
+    });
+  }
 }

3653973660424167d9a2da023126f85fe311dcf9
votienkhiem <votienkhiem@users.noreply.github.com>
2023-08-28 19:00:47 +0700

    add contact

--- src/app/Main/page/contact/contact.component.ts
+++ src/app/Main/page/contact/contact.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { faLocationDot, faPhoneVolume, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  faLocationDot = faLocationDot;
+  faPhoneVolume = faPhoneVolume;
+  faEnvelope = faEnvelope;
 
 }
